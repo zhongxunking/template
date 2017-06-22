0d4be22dfca98b3e2907c809f35eab45783de75c
zhongxunking <zhongxunking@163.com>
2017-06-22 14:37:44 +0800

    设置运行环境

--- template-assemble/src/main/java/org/antframework/template/assemble/Main.java
+++ template-assemble/src/main/java/org/antframework/template/assemble/Main.java
@@ -16,7 +16,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Main {
+    private static final String PROFILE_OS_PROPERTY_KEY = "SPRING_PROFILES_ACTIVE";
+    private static final String PROFILE_SYS_PROPERTY_KEY = "spring.profiles.active";
+
     public static void main(String[] args) {
+        setProfileIfNotExists("dev");
         SpringApplication.run(Main.class, args);
+    }
+
+    // 设置运行环境（如果未设置）
+    private static void setProfileIfNotExists(String profile) {
+        if (System.getenv(PROFILE_OS_PROPERTY_KEY) == null && System.getProperty(PROFILE_SYS_PROPERTY_KEY) == null) {
+            System.setProperty(PROFILE_SYS_PROPERTY_KEY, profile);
+        }
     }
 }
