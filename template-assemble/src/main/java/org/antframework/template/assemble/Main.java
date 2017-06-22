8b00a79136d5d8a8c6e33e5c7439c366a8109bef
zhongxunking <zhongxunking@163.com>
2017-06-22 12:59:04 +0800

    assemble模块新建启动类

---
+++ template-assemble/src/main/java/org/antframework/template/assemble/Main.java
@@ -0,0 +1,22 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2017-06-22 11:34 创建
+ */
+package org.antframework.template.assemble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序启动入口
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
