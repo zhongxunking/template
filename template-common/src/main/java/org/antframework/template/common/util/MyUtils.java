50110d479acd22850e81a396ef29d10dbeee80b2
zhongxun <zhongxunking@163.com>
2022-11-02 18:00:30 +0800

    更新

---
+++ template-common/src/main/java/org/antframework/template/common/util/MyUtils.java
@@ -0,0 +1,15 @@
+/*
+ * 作者：钟勋 (email:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2022-11-02 17:58 创建
+ */
+package org.antframework.template.common.util;
+
+/**
+ * 自定义工具类在此包下编写
+ */
+public class MyUtils {
+}
